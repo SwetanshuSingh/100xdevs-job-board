61aab35836f0c8ef2c85251a41dce647577a0bca
swetanshu <swetanshusingh17@gmail.com>
2024-08-03 10:47:01 +0530

    [feat]: add job card

---
+++ components/JobCard.tsx
@@ -0,0 +1,9 @@
+const JobCard = () => {
+    return (
+        <div className="min-h-24 w-full border border-gray-200 hover:border-gray-300 transition-all shadow-sm rounded-md px-4 py-3">
+            <h3>Testtt</h3>
+        </div>
+    )
+}
+
+export default JobCard;
