a685911ef4826444ec2ce7579df623b2358456c7
swetanshu <swetanshusingh17@gmail.com>
2024-08-04 01:27:18 +0530

    [feat]: add new job form and validation

--- components/forms/NewJobForm.tsx
+++ components/forms/NewJobForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import {
@@ -7,94 +8,216 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { NewJob, newJobSchema } from "@/zod/user";
+import { useToast } from "../ui/use-toast";
 
 const NewJobForm = () => {
+  const { toast } = useToast();
+
+  const form = useForm<NewJob>({
+    resolver: zodResolver(newJobSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      companyName: "",
+      salary: "",
+      currency: "",
+      location: "",
+    },
+  });
+
+  const handleFormSubmit = (values: NewJob) => {
+    const { currency, location } = values;
+
+    console.log(location)
+
+    if (currency !== "USD" && currency !== "INR") {
+      toast({
+        title: "Please select the currency",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    if (
+      location !== "REMOTE" &&
+      location !== "HYBRID" &&
+      location !== "OFFICE"
+    ) {
+      toast({
+        title: "Please select the location",
+        variant: "destructive",
+      });
+      return;
+    }
+  };
+
   return (
-    <form className="h-fit flex flex-col gap-3 p-2">
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-semibold text-gray-800" htmlFor="title">
-          Job Title *
-        </label>
-        <Input
-          name="title"
-          className="w-full border-gray-400"
-          placeholder="Job Title"
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <label
-          className="text-sm font-semibold text-gray-800"
-          htmlFor="description"
-        >
-          Job Description *
-        </label>
-        <Input
-          name="description"
-          className="w-full border-gray-400"
-          placeholder="Job Title"
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <label
-          className="text-sm font-semibold text-gray-800"
-          htmlFor="company_name"
-        >
-          Company Name *
-        </label>
-        <Input
-          name="comapany_name"
-          className="w-full border-gray-400"
-          placeholder="Company Name"
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-semibold text-gray-800" htmlFor="salary">
-          Salary *
-        </label>
-        <div className="flex justify-center items-center gap-2">
-          <Select>
-            <SelectTrigger className="w-24">
-              <SelectValue defaultValue="USD" placeholder="USD" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="USD">USD</SelectItem>
-              <SelectItem value="INR">INR</SelectItem>
-            </SelectContent>
-          </Select>
-          <Input
-            name="salary"
-            className="w-full border-gray-400"
-            placeholder="Salary"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleFormSubmit)}
+        className="h-fit flex flex-col gap-3 p-2"
+      >
+        <div className="flex flex-col gap-1">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm font-semibold text-gray-800">
+                  Job Title *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="w-full border-gray-400"
+                    placeholder="Job Title"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
         </div>
-      </div>
 
-      <div className="flex flex-col gap-1">
-        <label
-          className="text-sm font-semibold text-gray-800"
-          htmlFor="description"
-        >
-          Location *
-        </label>
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select loaction type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="REMOTE">Remote</SelectItem>
-            <SelectItem value="HYBRID">Hybird</SelectItem>
-            <SelectItem value="OFFICE">Office</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
+        <div className="flex flex-col gap-1">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm font-semibold text-gray-800">
+                  Description *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="w-full border-gray-400"
+                    placeholder="Enter description here"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
 
-      <div className="w-full flex justify-end items-center mt-4">
-        <Button>Create Job</Button>
-      </div>
-    </form>
+        <div className="flex flex-col gap-1">
+          <FormField
+            control={form.control}
+            name="companyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm font-semibold text-gray-800">
+                  Company Name *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="w-full border-gray-400"
+                    placeholder="Enter comapany name here"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label
+            className="text-sm font-semibold text-gray-800"
+            htmlFor="salary"
+          >
+            Salary *
+          </label>
+          <div className="flex justify-center items-center gap-2">
+            <FormField
+              control={form.control}
+              name="currency"
+              render={({ field }) => (
+                <FormItem>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-24">
+                        <SelectValue placeholder="select" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="USD">USD</SelectItem>
+                      <SelectItem value="INR">INR</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="salary"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="w-full border-gray-400"
+                      placeholder="Enter comapany name here"
+                    />
+                  </FormControl>
+                  <FormMessage className="absolute" />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm font-semibold text-gray-800">
+                  Location *
+                </FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select location type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="REMOTE">Remote</SelectItem>
+                    <SelectItem value="HYBRID">Hybird</SelectItem>
+                    <SelectItem value="OFFICE">Office</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="w-full flex justify-end items-center mt-4">
+          <Button type="submit">Create Job</Button>
+        </div>
+      </form>
+    </Form>
   );
 };
 
