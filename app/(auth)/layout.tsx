40753ad20647d503ef4902003062aa5c3a878612
Sahil-Gulihar <sahilgullihar@.com>
2024-08-03 13:54:55 +0530

    fixes loading

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ type AuthLayoutProps = {
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <div className="absolute inset-0 min-h-screen w-full flex flex-col items-center p-4 bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
-      <Navbar />
+      <Navbar session={null}/>
       {children}
       <Footer />
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,20 @@
 import { Copyright } from "lucide-react";
-
+import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full h-20 flex justify-between items-center bg-white/10 p-4 lg:px-14 border border-t-gray-200/90">
       <div className="flex justify-center items-center gap-4 font-normal text-slate-600">
+        <Link href='https://100xdevs.com'>
         <p>LinkedIn</p>
-        <p>Twitter</p>
-        <p>Github</p>
+        </Link>
+        
+        <Link href='https://x.com/swetanshu789'><p>Twitter</p>
+        </Link>
+        <Link href='https://github.com/swetanshusingh'><p>Github</p>
+        </Link>
+        <Link href='https://100xdevs.com'>
         <p>Website</p>
+        </Link>
       </div>
       <span className="flex text-xs font-normal text-slate-600 items-center gap-1">
         <Copyright size={14} />
